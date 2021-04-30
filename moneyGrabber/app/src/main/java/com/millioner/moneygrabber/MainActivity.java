5c34320f9ad5eb1223ed5c0c62ecd6e1d302b6e9
Maria <60785083+sereginamaria@users.noreply.github.com>
2021-04-30 23:11:02 +0300

    lr5

--- moneyGrabber/app/src/main/java/com/millioner/moneygrabber/MainActivity.java
+++ moneyGrabber/app/src/main/java/com/millioner/moneygrabber/MainActivity.java
@@ -1,7 +1,10 @@
 package com.millioner.moneygrabber;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,7 +12,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.io.IOUtils;
+import java.nio.charset.StandardCharsets;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -27,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         initRng();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,8 +48,35 @@ public class MainActivity extends AppCompatActivity {
         byte[] v = randomBytes(10);
     }
 
+//    protected void onButtonClick(View v) {
+//        TestHttpClient();
+//    }
+
     protected void onButtonClick(View v) {
-        TestHttpClient();
+        Intent it = new Intent(this, PinpadActivity.class);
+        startActivityForResult(it, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK || data != null) {
+                String pin = data.getStringExtra("pin");
+                Toast.makeText(this, pin, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public static byte[] StringToHex(String s){
+        byte[] hex;
+        try{
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex){
+            hex = null;
+        }
+        return hex;
     }
 
     protected void TestHttpClient(){
